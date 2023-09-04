e89b31b3122b557a9953d685b33e49c724eba03c
matanhaknezer <50666265+matanhaknezer@users.noreply.github.com>
2023-09-04 19:59:34 +0300

    form - style

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ const fields = [
 ];
 
 function App() {
-  const [accountType, setAccountType] = React.useState("Advanced");
-  const [useSSL, setUseSSL] = React.useState(true);
+  const [accountType, setAccountType] = useState("Advanced");
+  const [useSSL, setUseSSL] = useState(true);
   const [formData, setFormData] = useState({
     accountType: accountType,
     name: "",
@@ -52,27 +52,19 @@ function App() {
     useSSL: useSSL,
   });
 
- const isAdvanced = accountType === "Advanced";
- 
   useEffect(() => {
     // Update formData when accountType / useSSL are changed
-    if (accountType === "Advanced")
-    {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
+    const updatedFormData = {
+      ...formData,
       accountType: accountType,
-      useSSL: useSSL,
-    }))}
-    else
-    {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        accountType: accountType,
-        serverPath: undefined,
-        port: undefined,
-        useSSL: undefined,
-      }))}
+      serverPath: isAdvanced ? formData.serverPath : undefined,
+      port: isAdvanced ? formData.port : undefined,
+      useSSL: isAdvanced ? useSSL : undefined,
+    };
+    setFormData(updatedFormData);
   }, [accountType, useSSL]);
+
+  const isAdvanced = accountType === "Advanced";
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -83,70 +75,149 @@ function App() {
   };
 
   const handleSubmit = (e) => {
+    // perform form submission logic here, e.g., send data to a server
+    // For now, let's just log the form data to the console
     e.preventDefault();
     console.log(formData);
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+        maxWidth: "600px",
+        margin: "0 auto",
+        py: 6,
+      }}
+    >
       <form onSubmit={handleSubmit}>
-        <div>Account Type:</div>
-        <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={accountType}
-          onChange={(e) => setAccountType(e.target.value)}
-          autoWidth
+        <Box
+          sx={{
+            position: "relative",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
         >
-          <MenuItem value={"Advanced"}>Advanced</MenuItem>
-          <MenuItem value={"Manual"}>Manual</MenuItem>
-        </Select>
-        {fields.slice(0, 3).map((x) => (
-          <Box key={x.id} sx={{ flexDirection: "row" }}>
-            <div>{x.name + ":"}</div>
+          <div style={{ width: "20%", paddingRight: "10px" }}>
+            Account Type:
+          </div>
+          <Select
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={accountType}
+            onChange={(e) => setAccountType(e.target.value)}
+            autoWidth
+            style={{ width: "80%" }}
+          >
+            <MenuItem value={"Advanced"}>Advanced</MenuItem>
+            <MenuItem value={"Manual"}>Manual</MenuItem>
+          </Select>
+        </Box>
+
+        {fields.slice(0, 3).map((field) => (
+          <Box
+            key={field.id}
+            sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+          >
+            <div style={{ width: "20%", paddingRight: "10px" }}>
+              {field.name + ":"}
+            </div>
             <TextField
-              name={x.id}
-              value={formData[x.id]}
+              name={field.id}
+              value={formData[field.id]}
               onChange={handleChange}
               variant="outlined"
               required
               margin="normal"
-              type={x.type}
-              placeholder={x.placeholder}
+              type={field.type}
+              placeholder={field.placeholder}
+              style={{ width: "80%" }}
             />
           </Box>
         ))}
+
         {isAdvanced &&
-          fields.slice(3, 5).map((x) => (
-            <Box key={x.id} sx={{ flexDirection: "row" }}>
-              <div>{x.name + ":"}</div>
+          fields.slice(3, 4).map((field) => (
+            <Box
+              key={field.id}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ width: "20%", paddingRight: "10px" }}>
+                {field.name + ":"}
+              </div>
               <TextField
-                name={x.id}
-                value={formData[x.id]}
+                name={field.id}
+                value={formData[field.id]}
                 onChange={handleChange}
                 variant="outlined"
                 required
                 margin="normal"
-                placeholder={x.placeholder}
+                placeholder={field.placeholder}
+                style={{ width: "80%" }}
               />
             </Box>
           ))}
-        {isAdvanced && (
-          <>
-            <Checkbox
-              checked={useSSL}
-              onChange={(e) => setUseSSL(e.target.checked)}
-              inputProps={{ "aria-label": "controlled" }}
-            />{" "}
-            <div> Use SSL</div>
-          </>
-        )}
+
+        <Box sx={{ display: "flex", flexDirection: "row", gap: "40px" }}>
+          {isAdvanced &&
+            fields.slice(4, 5).map((field) => (
+              <Box
+                key={field.id}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ width: "50px", paddingRight: "10px" }}>
+                  {field.name + ":"}
+                </div>
+                <TextField
+                  name={field.id}
+                  value={formData[field.id]}
+                  onChange={handleChange}
+                  variant="outlined"
+                  required
+                  margin="normal"
+                  placeholder={field.placeholder}
+                />
+              </Box>
+            ))}
+          {isAdvanced && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ width: "90px", paddingRight: "10px" }}>
+                {" "}
+                Use SSL
+              </div>
+              <Checkbox
+                checked={useSSL}
+                onChange={(e) => setUseSSL(e.target.checked)}
+                inputProps={{ "aria-label": "controlled" }}
+                style={{ width: "80%" }}
+              />{" "}
+            </Box>
+          )}
+        </Box>
+
         <Button
           type="submit"
           variant="contained"
           color="primary"
           size="large"
-          style={{ marginTop: "20px" }}
+          style={{ marginTop: "20px", width: "100%" }}
         >
           Submit
         </Button>
