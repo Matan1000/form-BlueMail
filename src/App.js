7fb1ca294d9dc9e12a23dba34047dfc5e51926e4
matanhaknezer <50666265+matanhaknezer@users.noreply.github.com>
2023-09-12 14:55:26 +0300

    form - optimization & add features

--- src/App.js
+++ src/App.js
@@ -11,32 +11,35 @@ const fields = [
     id: "name",
     name: "User Name",
     placeholder: "name@example.com",
-    type: "text"
+    isRequired: true,
+    error: "user name should be an email"
   },
   {
     id: "password",
     name: "Password",
     placeholder: "Required",
-    type: "password"
+    isRequired: true,
+    error: "password must contain at least eight characters, at least one number and both lower and uppercase letters and special characters",
+    type: "password",
   },
   {
     id: "serverAdress",
     name: "Server Address",
     placeholder: "example.com",
-    type: "email"
+    error:"server adress should be a URL"
   },
   {
     id: "serverPath",
     name: "Server Path",
     placeholder: "/calendars/user/",
-    type: "url"
+    error:"please provide a valid path (contains alphanumeric characters and '/')"
   },
   {
     id: "port",
     name: "Port",
     placeholder: "",
-    type: "number"
-  }
+    error: "Please enter a valid Port (number)"
+  },
 ];
 
 function App() {
@@ -51,6 +54,8 @@ function App() {
     port: "",
     useSSL: useSSL,
   });
+  const [formErrors, setFormErrors] = useState({});
+  const [submitting,setSubmitting] =useState(false);
 
   useEffect(() => {
     // Update formData when accountType / useSSL are changed
@@ -76,34 +81,33 @@ function App() {
 
   const handleSubmit = (e) => {
     // perform form submission logic here, e.g., send data to a server
-    // For now, let's just log the form data to the console
+    // For now, let's just log the form data to the console & validate input values
     e.preventDefault();
+    setFormErrors(validateValues(formData));
+    setSubmitting(true);
     console.log(formData);
   };
 
   return (
     <Box
+      key={"fields"}
       sx={{
         display: "flex",
         flexDirection: "column",
         gap: "16px",
         maxWidth: "600px",
         margin: "0 auto",
-        py: 6,
+        border:2,
+        boxShadow: 3,
+        p:4,
+        borderColor:"azure", my:4
       }}
     >
-      <form onSubmit={handleSubmit}>
-        <Box
-          sx={{
-            position: "relative",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ width: "20%", paddingRight: "10px" }}>
-            Account Type:
-          </div>
+      {Object.keys(formErrors).length === 0 && submitting ? (
+<span>Your Application submitted successfully. Thanks!</span>
+      ): null}
+      <form onSubmit={handleSubmit} style={{display: !submitting ? "block" :"none"}}>
+        <Field fieldName={"Account Type"} fieldId={"accountType"}>
           <Select
             labelId="demo-simple-select-autowidth-label"
             id="demo-simple-select-autowidth"
@@ -115,43 +119,29 @@ function App() {
             <MenuItem value={"Advanced"}>Advanced</MenuItem>
             <MenuItem value={"Manual"}>Manual</MenuItem>
           </Select>
-        </Box>
+        </Field>
 
         {fields.slice(0, 3).map((field) => (
-          <Box
-            key={field.id}
-            sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-          >
-            <div style={{ width: "20%", paddingRight: "10px" }}>
-              {field.name + ":"}
-            </div>
+          <Field fieldName={field?.name} fieldId={field.id}>
             <TextField
               name={field.id}
               value={formData[field.id]}
               onChange={handleChange}
               variant="outlined"
-              required
+              required={field.isRequired}
               margin="normal"
               type={field.type}
               placeholder={field.placeholder}
               style={{ width: "80%" }}
+              error={formErrors[field?.id]}
+              helperText={formErrors[field?.id] ? field.error : undefined}
             />
-          </Box>
+          </Field>
         ))}
 
         {isAdvanced &&
           fields.slice(3, 4).map((field) => (
-            <Box
-              key={field.id}
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <div style={{ width: "20%", paddingRight: "10px" }}>
-                {field.name + ":"}
-              </div>
+            <Field fieldName={field.name} fieldId={field.id}>
               <TextField
                 name={field.id}
                 value={formData[field.id]}
@@ -161,24 +151,18 @@ function App() {
                 margin="normal"
                 placeholder={field.placeholder}
                 style={{ width: "80%" }}
-              />
-            </Box>
+                error={formErrors[field?.id]}
+                helperText={formErrors[field?.id] ? field.error : undefined}
+                // ^\/([a-zA-Z0-9\/]*)$
+                // inputProps={{ inputMode: 'text', pattern: '[a-zA-Z0-9\/]*' }}
+              /> 
+            </Field>
           ))}
 
         <Box sx={{ display: "flex", flexDirection: "row", gap: "40px" }}>
           {isAdvanced &&
             fields.slice(4, 5).map((field) => (
-              <Box
-                key={field.id}
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <div style={{ width: "50px", paddingRight: "10px" }}>
-                  {field.name + ":"}
-                </div>
+              <Field fieldName={field.name} fieldId={field.id}>
                 <TextField
                   name={field.id}
                   value={formData[field.id]}
@@ -187,28 +171,20 @@ function App() {
                   required
                   margin="normal"
                   placeholder={field.placeholder}
+                  error={formErrors[field?.id]}
+                  helperText={formErrors[field?.id] ? field.error : undefined}
                 />
-              </Box>
+              </Field>
             ))}
           {isAdvanced && (
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <div style={{ width: "90px", paddingRight: "10px" }}>
-                {" "}
-                Use SSL
-              </div>
+            <Field fieldName={"Use SSL"} fieldId={"useSSL"}>
               <Checkbox
                 checked={useSSL}
                 onChange={(e) => setUseSSL(e.target.checked)}
                 inputProps={{ "aria-label": "controlled" }}
                 style={{ width: "80%" }}
               />{" "}
-            </Box>
+            </Field>
           )}
         </Box>
 
@@ -227,3 +203,41 @@ function App() {
 }
 
 export default App;
+
+const Field = ({ fieldName, fieldId, children }) => {
+  return (
+    <Box
+      key={fieldId}
+      sx={{
+        position: "relative",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: fieldId !== "useSSL" ? "20%" : "90px",
+          paddingRight: "10px",
+        }}
+      >
+        {fieldId !== "useSSL" ? fieldName + ": " : fieldName}
+      </div>
+      {children}
+    </Box>
+  );
+};
+
+const validateValues = (inputValues) => {
+  let errors = {};
+  if (!inputValues?.name.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+  errors["name"] = true;
+  if (!inputValues?.password.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?=.*[a-zA-Z]).{8,}$"))
+  errors["password"] =true
+  if (!inputValues?.serverAdress.match("^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$"))
+  errors["serverAdress"] =true
+  if (!inputValues?.serverPath.match("^[a-zA-Z0-9/]*$"))
+  errors["serverPath"] =true;
+if(!inputValues?.port.match("^[0-9]+$")){errors["port"]=true}
+return errors
+}
